171a3d3d5b50edb16af7e3b09cf89b04ca2d79a5
关鑫贤 <guanxinxian@guanxinxiandeMacBook-Pro.local>
2016-01-11 17:32:19 +0800

    use google analytics to replace baidu

--- src/js/index.js
+++ src/js/index.js
@@ -28,6 +28,13 @@
             }
         });
         //scroll to change the header style - end
+        
+        //
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', gxxconf.googleanalyticsid, 'auto');
 
         //Init when onload or pjax success - start
         function gxxPostInit() {
@@ -53,14 +60,8 @@
                     hljs.highlightBlock(block);
                 });
 
-                //百度统计
-                var _hmt = _hmt || [];
-                (function() {
-                    var hm = document.createElement("script");
-                    hm.src = "//hm.baidu.com/hm.js?"+gxxconf.baiduhmid;
-                    var s = document.getElementsByTagName("script")[0]; 
-                    s.parentNode.insertBefore(hm, s);
-                })();
+                //Google Analytics support
+                ga('send', 'pageview');
             }
         }
         gxxPostInit();
